f5bf2c53549069f5b16017027a223ae85981221a
Витек <vitekb650@gmail.com>
2024-03-31 18:50:34 +0300

    refactor: mapping

--- search/src/main/java/ru/bookinbl/search/myservice/controller/BookingController.java
+++ search/src/main/java/ru/bookinbl/search/myservice/controller/BookingController.java
@@ -6,7 +6,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.bookinbl.search.myservice.model.Booking;
@@ -21,7 +20,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/MyBookings")
 @Slf4j
 @Validated
 public class BookingController {
@@ -32,7 +30,7 @@ public class BookingController {
     public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
 
-    @GetMapping
+    @GetMapping("/MyBookings")
     public List<Booking> getBookings(@RequestParam(required = false, defaultValue = "") String city,
                                      @RequestParam(required = false, defaultValue = "0") Integer rating,
                                      @RequestParam(defaultValue = "0") @PositiveOrZero int from,
@@ -41,7 +39,7 @@ public class BookingController {
         return bookingService.getBookings(city, rating, from, size);
     }
 
-    @GetMapping("/time")
+    @GetMapping("/MyBookings/time")
     public ResponseEntity<Object> getBookingsWithTime(@RequestParam(required = false, defaultValue = "#{T(java.time.LocalDateTime).now()}") @DateTimeFormat(pattern = DATETIME_FORMAT) LocalDateTime rangeStart,
                                                       @RequestParam(required = false) @DateTimeFormat(pattern = DATETIME_FORMAT) LocalDateTime rangeEnd) {
         log.debug("Вызван метод getBookingsWithTime");
