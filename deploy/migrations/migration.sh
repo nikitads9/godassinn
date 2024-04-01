b6d49eac48cfb33cc1d79a100807608d722d97f3
Nikita Denisenok <nikitadenisenok@gmail.com>
2024-04-01 15:03:53 +0300

    updated migrator

--- deploy/migrations/migration.sh
+++ deploy/migrations/migration.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sleep 2 && goose -dir . up -v
+export GOOSE_DBSTRING="host=${DB_HOST} port=${DB_PORT} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASSWORD} sslmode=${DB_SSL}"
+export GOOSE_MIGRATION_DIR=.
+sleep 2 && goose up -v
